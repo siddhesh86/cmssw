22acf41139625520351b176b48ffcaf523f8da8e
Siddhesh Sawant <siddhesh.gopichand.sawant@cern.ch>
2018-11-22 16:26:17 +0100

    Use GaussVtxSmearingParameters insteam of VtxSmearedCommon in VtxSmearedHLLHC14TeV_cfi.py

--- IOMC/EventVertexGenerators/python/VtxSmearedHLLHC14TeV_cfi.py
+++ IOMC/EventVertexGenerators/python/VtxSmearedHLLHC14TeV_cfi.py
@@ -4,7 +4,8 @@ from IOMC.EventVertexGenerators.VtxSmearedParameters_cfi import *
 HLLHCVtxSmearingParameters14TeV = HLLHCVtxSmearingParameters.clone( EprotonInGeV = cms.double(7000) )
 VtxSmeared = cms.EDProducer("HLLHCEvtVtxGenerator",
     HLLHCVtxSmearingParameters14TeV,
-    VtxSmearedCommon
+#    VtxSmearedCommon
+    GaussVtxSmearingParameters
 )
 
 
